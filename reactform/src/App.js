41161bc58a105abd0f05963e5d0518cdb6fb81bd
ruivalente99 <rui.valente99@gmail.com>
2022-03-30 01:36:18 +0100

    Create theme

--- reactform/src/App.js
+++ reactform/src/App.js
@@ -1,12 +1,38 @@
 import React from 'react'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import ThemeToggle from './components/Form/ThemeToggle'
 import './App.css'
 import { Form } from './components/Form'
-function App () {
+
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} })
+
+export default function App () {
+  const [mode, setMode] = React.useState('light')
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'))
+      }
+    }),
+    []
+  )
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode
+        }
+      }),
+    [mode]
+  )
+
   return (
-    <div>
-      <Form />
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <ThemeToggle context={ColorModeContext} />
+        <Form />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
-
-export default App
